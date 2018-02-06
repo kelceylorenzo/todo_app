aca16d402013e29c0c9d6c9b873f984f70ae4890
m13kelore <kelceylorenzo@gmail.com>
2018-02-06 12:36:53 -0800

    Initial app set up with dummy data completed

---
+++ src/assets/data/index.js
@@ -0,0 +1,18 @@
+export default [
+	{
+		title: 'First to do item',
+		complete: false
+	},
+	{
+		title: 'Second to do item',
+		complete: false
+	},
+	{
+		title: 'Third to do item',
+		complete: false
+	},
+	{
+		title: 'Fourth to do item',
+		complete: false
+	}
+];
