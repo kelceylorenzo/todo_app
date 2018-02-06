4c49089adf8194eaba4deaa0934cc0a969f543f4
m13kelore <kelceylorenzo@gmail.com>
2018-02-06 14:28:18 -0800

    add items complete

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TodoList from './todo_list';
+import AddForm from './add_form';
+import todoData from '../assets/data';
 import 'materialize-css/dist/css/materialize.min.css';
 
-const App = () => (
-	<div className="container">
-		<h1 className="center-align">To Do App</h1>
-		<TodoList />
-	</div>
-);
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			list: todoData
+		};
+
+		this.addItem = this.addItem.bind(this);
+	}
+
+	addItem(item) {
+		this.setState({
+			list: [item, ...this.state.list]
+		});
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<h1 className="center-align">To Do App</h1>
+				<AddForm add={this.addItem} />
+				<TodoList list={this.state.list} />
+			</div>
+		);
+	}
+}
 
 export default App;
