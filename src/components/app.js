5053a5120e5edb518e74abc06bf3a87f28e2eea1
m13kelore <kelceylorenzo@gmail.com>
2018-02-06 18:59:10 -0800

    toggle complete

--- src/components/app.js
+++ src/components/app.js
@@ -1,34 +1,52 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import TodoList from './todo_list';
 import AddForm from './add_form';
-import todoData from '../assets/data';
 import 'materialize-css/dist/css/materialize.min.css';
+
+const BASE_URL = 'http://api.reactprototypes.com';
+const API_KEY = '?key=kelceyl4769';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			list: todoData
+			list: []
 		};
 
 		this.addItem = this.addItem.bind(this);
 		this.deleteItem = this.deleteItem.bind(this);
+		this.toggleComplete = this.toggleComplete.bind(this);
 	}
 
-	addItem(item) {
-		this.setState({
-			list: [item, ...this.state.list]
+	componentDidMount() {
+		this.getData();
+	}
+
+	getData() {
+		axios.get(`${BASE_URL}/todos${API_KEY}`).then((resp) => {
+			this.setState({
+				list: resp.data.todos
+			});
 		});
 	}
 
-	deleteItem(index) {
-		const newList = this.state.list.slice();
+	addItem(item) {
+		axios.post(`${BASE_URL}/todos${API_KEY}`, item).then((resp) => {
+			this.getData();
+		});
+	}
 
-		newList.splice(index, 1);
+	deleteItem(id) {
+		axios.delete(`${BASE_URL}/todos/${id + API_KEY}`).then((resp) => {
+			this.getData();
+		});
+	}
 
-		this.setState({
-			list: newList
+	toggleComplete(id) {
+		axios.put(`${BASE_URL}/todos/${id + API_KEY}`).then((resp) => {
+			this.getData();
 		});
 	}
 
@@ -37,7 +55,7 @@ class App extends Component {
 			<div className="container">
 				<h1 className="center-align">To Do App</h1>
 				<AddForm add={this.addItem} />
-				<TodoList list={this.state.list} delete={this.deleteItem} />
+				<TodoList list={this.state.list} delete={this.deleteItem} toggle={this.toggleComplete} />
 			</div>
 		);
 	}
